65d2951d3ad75225dd825e990598a2f5d586ac52
thorveblenred <thorveblenred@gmail.com>
2023-10-28 10:09:20 -0400

    first version of pointer summary example --completed

---
+++ Pointer summary/pointer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int doSomething(int& x, int& y);
+int doSomething2(int* x, int* y);
+int main() {
+	int x = 1, y = 1;
+	cout << doSomething(x,y);
+	x = 1; y = 1;
+	cout << doSomething2(&x, &y);
+
+
+}
+/*what is done here*/
+int doSomething(int& x, int& y) {
+	int temp = x;
+	x = y + 10;
+	y = temp * 10;
+	return x + y;
+}
+int doSomething2(int* x, int* y) {
+	int temp = *x;
+	*x = *y + 10;
+	*y = temp * 10;
+	return (*x + *y);
+}
